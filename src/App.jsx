6f4d40a981de75bb88e247b27bedf8a182415b6c
emonShimoul <emonshimoul@gmail.com>
2026-09-24 19:04:24 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center mt-10 text-red-500 text-3xl font-bold">
+      Hello Tasker
+    </div>
+  );
+}
+
+export default App;
